8a51e019b5945001bb76c4ae92a142027fcca675
이준희 <joonnc@yonsei.ac.kr>
2020-11-20 14:15:40 +0900

    Added PropertyLogger class to print all Environmental variables(including properties)
    @EventListener
     - handleContextRefresh() function is called when ContextRefreshedEvent occurs.

--- src/main/java/kr/or/kftc/scratch/SpringBootScratchApplication.java
+++ src/main/java/kr/or/kftc/scratch/SpringBootScratchApplication.java
@@ -1,6 +1,7 @@
 package kr.or.kftc.scratch;
 
 import kr.or.kftc.scratch.configuration.AboutMe;
+import kr.or.kftc.scratch.configuration.PropertyLogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
